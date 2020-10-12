06f39bc1a5aebd1ab452783d92ebfbf074e7ed44
Tim Bannister <tim@scalefactory.com>
2020-10-12 09:19:53 +0100

    Allow setting AWS profile via command line parameter

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,15 @@ mod s3;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let app = clap::App::new(clap::crate_name!())
+      .version(clap::crate_version!())
+      .about(clap::crate_description!())
+      .arg(clap::Arg::from_usage("-p, --profile=[NAME] 'Provides an input file to the program'"));
+
+    if let Some(profile_name) = app.get_matches().value_of("profile") {
+        std::env::set_var("AWS_PROFILE",&*profile_name);
+    }
+
     let client = s3::Client::new();
 
     client.report_all().await?;
