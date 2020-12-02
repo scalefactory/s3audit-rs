fa98b66f80cdb6892aa7ff05a3853d96de2b4ae3
David O'Rourke <david@scalefactory.com>
2020-12-02 20:17:06 +0000

    policy: Implement actions checking

--- src/s3/bucket_policy.rs
+++ src/s3/bucket_policy.rs
@@ -14,6 +14,63 @@ pub enum BucketPolicy {
 }
 
 #[derive(Debug)]
+struct Action(Vec<String>);
+
+impl Action {
+    fn wildcards(&self) -> usize {
+        // Wildcards could appear anywhere in the ARN
+        // eg. "*", "s3:*", "iam:*AccessKey*"
+        self.0.iter()
+            .filter(|&arn| arn.contains(WILDCARD))
+            .count()
+    }
+}
+
+#[cfg(test)]
+impl PartialEq for Action {
+    fn eq(&self, other: &Self) -> bool {
+        let mut first = self.0.clone();
+        first.sort();
+
+        let mut second = other.0.clone();
+        second.sort();
+
+        first == second
+    }
+}
+
+// Takes a Value representing the Principal entry in a Bucket Policy and
+// returns a Vec of the discovered ARNs wrapped in a Principal struct.
+impl From<&Value> for Action {
+    fn from(value: &Value) -> Self {
+        let output = match value {
+            // "Action": "s3:Foo"
+            Value::String(arn) => {
+                let arns = vec![
+                    String::from(arn),
+                ];
+
+                Self(arns)
+            },
+            // "Action": [
+            //   "s3:Bar",
+            //   "s3:Foo",
+            // ]
+            Value::Array(actions) => {
+                let actions: Vec<String> = actions.iter()
+                    .map(|s| String::from(s.as_str().unwrap()))
+                    .collect();
+
+                Self(actions)
+            },
+            _ => Self(Vec::new()),
+        };
+
+        output
+    }
+}
+
+#[derive(Debug)]
 struct Principal(Vec<String>);
 
 impl Principal {
@@ -100,6 +157,13 @@ impl From<GetBucketPolicyOutput> for BucketPolicy {
 
         let mut wildcard_statements_total: usize = 0;
 
+        // Process the actions.
+        let action = &jv["Action"];
+        let action: Action = action.into();
+        let action_wildcards = action.wildcards();
+
+        wildcard_statements_total += action_wildcards;
+
         // Process the principals.
         let principal = &jv["Principal"];
         let principal: Principal = principal.into();
@@ -158,6 +222,54 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
     use serde_json::json;
+
+    #[test]
+    fn test_from_policy_action_string() {
+        let policy = json!({
+            "Effect": "Allow",
+            "Action": "*",
+            "Principal": "*",
+        });
+
+        let action = &policy["Action"];
+        let action: Action = action.into();
+        let expected = vec!["*"];
+
+        assert_eq!(action.0, expected);
+        assert_eq!(action.wildcards(), 1);
+    }
+
+    #[test]
+    fn test_from_policy_action_vec() {
+        let policy = json!({
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets",
+                "s3:GetObject",
+                "s3:*",
+                "*",
+            ],
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::123456789012:root",
+                    "123456789012",
+                    "*",
+                ],
+            },
+        });
+
+        let action = &policy["Action"];
+        let action: Action = action.into();
+        let expected = Action(vec![
+            "s3:GetObject".into(),
+            "s3:ListAllMyBuckets".into(),
+            "s3:*".into(),
+            "*".into(),
+        ]);
+
+        assert_eq!(action, expected);
+        assert_eq!(action.wildcards(), 2);
+    }
 
     #[test]
     fn test_from_policy_principal_string() {
